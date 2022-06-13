873b78ab2e27350bb9fc2f87c8ce1e829465729b
Freespirit <66442151+Jose-Tom@users.noreply.github.com>
2022-06-13 22:58:16 +0530

    built homepage

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const expressLayout = require("express-ejs-layouts");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Assets
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
