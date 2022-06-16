d1d408875d7214718655e10024ccbd1c0b0eb2c8
Freespirit <66442151+Jose-Tom@users.noreply.github.com>
2022-06-16 14:36:40 +0530

    authentication

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("express-flash");
 const MongoDbStore = require("connect-mongo");
+const passport = require("passport");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -41,12 +42,24 @@ app.use(flash());
 
 // Assets
 app.use(express.static("public"));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 app.use(expressLayout);
+
+// Passport config
+const passportInit = require("./app/config/passport");
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Global middleware
 app.use((req, res, next) => {
   res.locals.session = req.session;
+  res.locals.user = req.user;
   next();
 });
 
